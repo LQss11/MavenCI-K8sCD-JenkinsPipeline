8a7cc1da1d7725950224fd3201ffc134f9053be6
LQss11 <49565791+LQss11@users.noreply.github.com>
2022-02-04 03:14:54 +0100

    update script execution for cluster generation

--- go/setup-kinds.sh
+++ go/setup-kinds.sh
@@ -5,30 +5,43 @@ cat >kind-config.yaml <<EOF
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 nodes:
-- role: control-plane
 EOF
-
+# Regular exp not between 1 and 9
 re='^[1-9]+$'
-while read -p "How many manager nodes do you need in the cluster?:" node; do
-    if ! [[ $node =~ $re ]]; then
+# Select how many manager and worker nodes
+# to be in the kind cluster
+# 1 control-plane and 1 worker for minimal test
+while read -p "How many manager nodes do you need in the cluster?:" manager; do
+    if ! [[ $manager =~ $re ]]; then
         echo "error: Not a valid number" >&2
         exit 1
     fi
-    for i in $(seq 1 $node); do
+    for i in $(seq 1 $manager); do
         echo - role: control-plane >>kind-config.yaml
     done
     break
 done
 
-while read -p "How many worker nodes do you need in the cluster?:" node; do
-    if ! [[ $node =~ $re ]]; then
+while read -p "How many worker nodes do you need in the cluster?:" worker; do
+    if ! [[ $worker =~ $re ]]; then
         echo "error: Not a valid number" >&2
         exit 1
     fi
-    for i in $(seq 1 $node); do
+    for i in $(seq 1 $worker); do
         echo - role: worker >>kind-config.yaml
     done
     break
 done
+
+# Create the cluster
+kind create cluster --name k8s-cluster --config kind-config.yaml
+
+# Update the k8s server api host+port depending on 
+# haproxy loadbalancerr or single control-plane 
+if [[ $manager == 1 ]]; then
+    kubectl config set clusters.kind-k8s-cluster.server https://k8s-cluster-control-plane:6443
+else
+    kubectl config set clusters.kind-k8s-cluster.server https://k8s-cluster-external-load-balancer:6443
+fi
 
 # kind create cluster --name k8s-playground --config kind-config.yaml
