34ba0247d0f86ced1e35a5276bf7f5af4a2ebeff
LQss11 <49565791+LQss11@users.noreply.github.com>
2022-02-06 03:13:05 +0100

    Remove check response code 200

--- cluster/content/scripts/deploy-nginx.sh
+++ cluster/content/scripts/deploy-nginx.sh
@@ -9,4 +9,4 @@ echo "Waiting for nginx pods STATUS to be RUNNING"
 kubectl wait --for=jsonpath='{.status.phase}'=Running pod -l app=nginx1.14.2 
 # Wait untill nginx web app curl response code 200 (timeout in 20s if not 200)
 echo "Waiting for nginx web app response code 200 (timeout 20)"
-timeout 60 sh -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' k8s-cluster-control-plane:30080)" != "200" ]]; do sleep 5; done && echo nginx app running'
+#timeout 60 sh -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' k8s-cluster-control-plane:30080)" != "200" ]]; do sleep 5; done && echo nginx app running'
