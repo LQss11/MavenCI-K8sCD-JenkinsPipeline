bd997ac91bc776c4cc1a64626cc9060b7b4b8d58
LQss11 <49565791+LQss11@users.noreply.github.com>
2022-02-08 17:56:39 +0100

    load images into cluster

--- cluster/content/scripts/deploy.sh
+++ cluster/content/scripts/deploy.sh
@@ -2,9 +2,17 @@
 # Pull required images
 docker image pull nginx:1.14.2
 docker image pull jenkins/jenkins:lts
+docker image pull mysql:5.7.32
+docker image pull sonatype/nexus3:3.37.0
+docker image pull phpmyadmin/phpmyadmin:5.1.1
+docker image pull sonarqube:7.6-community
 # Load downloaded images to the cluster nodes
 kind load docker-image nginx:1.14.2 --name k8s-cluster
 kind load docker-image jenkins/jenkins:lts --name k8s-cluster
+kind load docker-image mysql:5.7.32 --name k8s-cluster
+kind load docker-image sonatype/nexus3:3.37.0 --name k8s-cluster
+kind load docker-image phpmyadmin/phpmyadmin:5.1.1 --name k8s-cluster
+kind load docker-image sonarqube:7.6-community --name k8s-cluster
 # Apply deployments manifests
 kubectl apply -f ./content/deployments/
 # Wait for pods status=running
