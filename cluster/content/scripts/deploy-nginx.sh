bc1bd1964959e860d4f171e3b789890fce6d1b27
LQss11 <49565791+LQss11@users.noreply.github.com>
2022-02-08 17:14:30 +0100

    ignore checking for response code 200

--- cluster/content/scripts/deploy-nginx.sh
+++ cluster/content/scripts/deploy-nginx.sh
@@ -11,6 +11,6 @@ kubectl apply -f ./content/deployments/
 echo "Waiting for pods STATUS to be RUNNING with spefied label..."
 kubectl wait --for=jsonpath='{.status.phase}'=Running pod -l workload=cicd  
 # Wait untill app curl on specified port produces response code 200 (timeout if not 200)
-echo "checking for jenkins app..."
-timeout 200 sh -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' k8s-cluster-control-plane:30082)" != "200" ]]; do sleep 5; done && echo jenkins app running'
+#echo "checking for jenkins app..."
+#timeout 200 sh -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' k8s-cluster-control-plane:30082)" != "200" ]]; do sleep 5; done && echo jenkins app running'
 
