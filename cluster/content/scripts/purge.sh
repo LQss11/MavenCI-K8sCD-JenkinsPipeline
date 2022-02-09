31fe06088535a2c9a04062afa59ff325f6f54abd
LQss11 <49565791+LQss11@users.noreply.github.com>
2022-02-09 22:51:52 +0100

    delete cluster

--- cluster/content/scripts/purge.sh
+++ cluster/content/scripts/purge.sh
@@ -5,3 +5,6 @@ kubectl delete -f ./content/deployments/
 kubectl delete -f ./content/deployments/secrets
 # delete storages
 kubectl delete -f ./content/deployments/storage
+
+# Delete Cluster
+kind delete cluster --name k8s-cluster
