8d4ca9472619759daf9275a6abb8070fb45a5c2a
LQss11 <49565791+LQss11@users.noreply.github.com>
2022-02-09 01:23:33 +0100

    update wait running condition timeout to 3 mins

--- cluster/content/scripts/deploy.sh
+++ cluster/content/scripts/deploy.sh
@@ -19,7 +19,7 @@ kubectl apply -f ./content/deployments/secrets
 kubectl apply -f ./content/deployments/
 # Wait for pods status=running
 echo "Waiting for pods STATUS to be RUNNING with spefied label..."
-kubectl wait --for=jsonpath='{.status.phase}'=Running pod -l workload=cicd  
+kubectl wait --for=jsonpath='{.status.phase}'=Running pod -l workload=cicd  --timeout 3m # Will stop if reach timeout
 # Wait untill app curl on specified port produces response code 200 (timeout if not 200)
 #echo "checking for jenkins app..."
 #timeout 200 sh -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' k8s-cluster-control-plane:30082)" != "200" ]]; do sleep 5; done && echo jenkins app running'
