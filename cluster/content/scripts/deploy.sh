311829a3137d2a9775a476014b613a20fecf57da
LQss11 <49565791+LQss11@users.noreply.github.com>
2022-02-09 01:32:27 +0100

    remove additional commented lines

--- cluster/content/scripts/deploy.sh
+++ cluster/content/scripts/deploy.sh
@@ -1,18 +1,10 @@
 #!/bin/sh
+
 # Pull required images
 # docker image pull nginx:1.14.2
-# docker image pull jenkins/jenkins:lts
-# docker image pull mysql:5.7.32
-# docker image pull sonatype/nexus3:3.37.0
-# docker image pull phpmyadmin/phpmyadmin:5.1.1
-# docker image pull sonarqube:7.6-community
 # # Load downloaded images to the cluster nodes
 # kind load docker-image nginx:1.14.2 --name k8s-cluster
-# kind load docker-image jenkins/jenkins:lts --name k8s-cluster
-# kind load docker-image mysql:5.7.32 --name k8s-cluster
-# kind load docker-image sonatype/nexus3:3.37.0 --name k8s-cluster
-# kind load docker-image phpmyadmin/phpmyadmin:5.1.1 --name k8s-cluster
-# kind load docker-image sonarqube:7.6-community --name k8s-cluster
+
 # Apply secrets
 kubectl apply -f ./content/deployments/secrets
 # Apply deployments manifests
