aec4b8ee0718bf45a5f2e69bb32c4f6e346809d5
LQss11 <49565791+LQss11@users.noreply.github.com>
2022-02-12 00:43:29 +0100

    setup init jenkins script

---
+++ cluster/jenkins-scripts/init.groovy
@@ -0,0 +1,42 @@
+import jenkins.model.*
+import hudson.security.*
+import jenkins.security.s2m.AdminWhitelistRule
+
+def env = System.getenv()
+def instance = Jenkins.getInstance()
+
+// Initial username and password using env variables
+println "--> Initial username and password using env variables"
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount(env.JENKINS_USER, env.JENKINS_PASS)
+instance.setSecurityRealm(hudsonRealm)
+
+// associate the created user to become admin
+println "--> Associate the created user to become admin"
+def strategy1 = new GlobalMatrixAuthorizationStrategy()
+strategy1.add(Jenkins.ADMINISTER, env.JENKINS_USER)
+instance.setAuthorizationStrategy(strategy1)
+
+// logged-in users can do anything
+println "--> Checking logged-in users can do anything"
+def strategy2 = new hudson.security.FullControlOnceLoggedInAuthorizationStrategy()
+strategy2.setAllowAnonymousRead(false)
+instance.setAuthorizationStrategy(strategy2)
+
+
+// Set Agent to controller security subsystem on
+println "--> Enabling slave master access control"
+Jenkins.instance.injector.getInstance(AdminWhitelistRule.class)
+    .setMasterKillSwitch(false);
+
+// save current Jenkins state to disk
+println "--> Saving instance"
+instance.save()
+
+// Set the default URL
+// TODO : Update  for dynamic port
+println "--> Setting default URL"
+def jlc = JenkinsLocationConfiguration.get()
+//jlc.setUrl("http://localhost:"+env.JENKINS_PORT+"/")
+jlc.setUrl("http://localhost:20080/")
+jlc.save()
